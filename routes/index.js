bfc44430cd5db47014693d944f21b93fc3464090
Laud Gilbert <laudaccurate@gmail.com>
2023-03-24 11:41:40 +0000

    Admin model and basic routes

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 const {
-  createUser,
-  login,
-  getUsers,
-  getUser,
-  deleteUser,
-} = require("./handlers");
+	createUser,
+	login,
+	getUsers,
+	getUser,
+	deleteUser,
+} = require("./users.handlers");
 
 module.exports = (app) => {
-  app.get("/", (req, res, next) => {
-    console.log("request sent to / route");
-    res.json({ success: "OK" });
-  });
+	app.get("/", (req, res, next) => {
+		console.log("request sent to / route");
+		res.json({ success: "OK" });
+	});
 
-  app.post("/login", login);
+	app.post("/login", login);
 
-  app.post("/create-user", createUser);
+	app.post("/create-user", createUser);
 
-  app.get("/users", getUsers);
+	app.get("/users", getUsers);
 
-  //Get a user
-  app.get("/user/:id", getUser);
+	//Get a user
+	app.get("/user/:id", getUser);
 
-  //Delete a user
-  app.delete("/user/:id", deleteUser);
+	//Delete a user
+	app.delete("/user/:id", deleteUser);
 };
