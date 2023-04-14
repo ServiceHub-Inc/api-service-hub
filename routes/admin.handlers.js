9092da1b3c8515c74b9929d5d50fa4bc3159d88a
Laud Gilbert <laudaccurate@gmail.com>
2023-04-14 11:24:59 +0000

    recording last login and login attempts

--- routes/admin.handlers.js
+++ routes/admin.handlers.js
@@ -8,141 +8,160 @@ const jwt = require("jsonwebtoken");
 
 //Creating a Token function
 const createToken = (_id, expiresIn) => {
-  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn });
+	return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn });
 };
 
 //
 //Setting Up Multer MiddleWare
 const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: function (req, file, cb) {
-    cb(null, uuid.v4() + "-" + file.originalname);
-  },
+	destination: "./public/uploads/",
+	filename: function (req, file, cb) {
+		cb(null, uuid.v4() + "-" + file.originalname);
+	},
 });
 const upload = multer({ storage: storage });
 
 exports.loginAdmin = async (req, res, next) => {
-  console.log("logging in");
-  try {
-    const { email, password } = req.body;
+	console.log("logging in");
+	try {
+		const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.json({ error: "All fields required" });
-    }
+		if (!email || !password) {
+			return res.json({ error: "All fields required" });
+		}
 
-    const admin = await Admin.findOne({ email });
-    if (!admin) {
-      return res
-        .status(400)
-        .json({ message: "The email or password you entered is incorrect" });
-    }
+		const admin = await Admin.findOne({ email });
+		if (!admin) {
+			return res
+				.status(400)
+				.json({ message: "The email or password you entered is incorrect" });
+		}
 
-    const matching = await admin.authenticate(password);
-    if (!matching) {
-      return res
-        .status(400)
-        .json({ message: "The email or password you entered is incorrect" });
-    }
+		const matching = await admin.authenticate(password);
+		if (!matching) {
+			if (admin.login_attempts < 5) {
+				// record the login_attempts
+				Admin.updateOne(
+					{ id: admin._id },
+					{ login_attempts: admin.login_attempts + 1 }
+				);
 
-    // Token expiration time (in seconds)
-    const expiresIn = 3600; // 1 hour
+				return res
+					.status(400)
+					.json({ message: "The email or password you entered is incorrect" });
+			} else {
+				return res.status(400).json({
+					message:
+						"Too many failed login attempts. Please try again after 30 minutes",
+				});
+			}
+		}
 
-    // Add a token
-    const token = createToken(admin._id, expiresIn);
+		// record the last_login
+		Admin.updateOne(
+			{ id: admin._id },
+			{ login_attempts: 0, last_login: Date.now() }
+		);
 
-    // Calculate tokenValidity in milliseconds
-    const tokenValidity = Date.now() + expiresIn * 1000;
+		// Token expiration time (in seconds)
+		const expiresIn = 3600; // 1 hour
 
-    const { password: pass, ...rest } = admin;
-    return res.status(200).json({ admin, token, tokenValidity });
-  } catch (error) {
-    return res.status(error.status || 401).json({ message: error.message });
-  }
+		// Add a token
+		const token = createToken(admin._id, expiresIn);
+
+		// Calculate tokenValidity in milliseconds
+		const tokenValidity = Date.now() + expiresIn * 1000;
+
+		const { password: pass, ...rest } = admin;
+		return res.status(200).json({ admin, token, tokenValidity });
+	} catch (error) {
+		return res.status(error.status || 401).json({ message: error.message });
+	}
 };
 
 //Creating an Admin
 exports.createAdmin = async (req, res, next) => {
-  console.log("Create admin handler");
+	console.log("Create admin handler");
 
-  try {
-    upload.single("image")(req, res, async function (err) {
-      if (err) {
-        return res
-          .status(400)
-          .json({ error: err.message, cMsg: "Error creating admin" });
-      }
+	try {
+		upload.single("image")(req, res, async function (err) {
+			if (err) {
+				return res
+					.status(400)
+					.json({ error: err.message, cMsg: "Error creating admin" });
+			}
 
-      //Assigning file properties
-      const file = req.file;
-      const imageUrl = `uploads/${file?.filename}`; // get the path of the uploaded image
+			//Assigning file properties
+			const file = req.file;
+			const imageUrl = `uploads/${file?.filename}`; // get the path of the uploaded image
 
-      const { email, phone } = req.body;
+			const { email, phone } = req.body;
 
-      // Checking if email or phone already exists
-      const emailExists = await Admin.findOne({ email });
-      if (emailExists) {
-        return res.status(400).json({
-          error: "Email already exists",
-          cMsg: "Error creating admin",
-        });
-      }
-      //Checking if phone exists
-      const phoneExists = await Admin.findOne({ phone });
-      if (phoneExists) {
-        return res.status(400).json({
-          error: "Phone number already exists",
-          cMsg: "Error creating admin",
-        });
-      }
+			// Checking if email or phone already exists
+			const emailExists = await Admin.findOne({ email });
+			if (emailExists) {
+				return res.status(400).json({
+					error: "Email already exists",
+					cMsg: "Error creating admin",
+				});
+			}
+			//Checking if phone exists
+			const phoneExists = await Admin.findOne({ phone });
+			if (phoneExists) {
+				return res.status(400).json({
+					error: "Phone number already exists",
+					cMsg: "Error creating admin",
+				});
+			}
 
-      const adminData = req.body;
+			const adminData = req.body;
 
-      const rememberToken = uuid.v4();
-      const admin = await Admin.create({
-        ...adminData,
-        image: file?.filename,
-        imageUrl,
-        remember_token: rememberToken,
-      });
+			const rememberToken = uuid.v4();
+			const admin = await Admin.create({
+				...adminData,
+				image: file?.filename,
+				imageUrl,
+				remember_token: rememberToken,
+			});
 
-      // Token expiration time (in seconds)
-      const expiresIn = 3600; // 1 hour
+			// Token expiration time (in seconds)
+			const expiresIn = 3600; // 1 hour
 
-      // Add a token
-      const token = createToken(admin._id, expiresIn);
+			// Add a token
+			const token = createToken(admin._id, expiresIn);
 
-      // Calculate tokenValidity in milliseconds
-      const tokenValidity = Date.now() + expiresIn * 1000;
+			// Calculate tokenValidity in milliseconds
+			const tokenValidity = Date.now() + expiresIn * 1000;
 
-      res.status(200).json({ admin, token, tokenValidity });
-    });
-  } catch (error) {
-    next(error);
-  }
+			res.status(200).json({ admin, token, tokenValidity });
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 //testing email
 exports.sendMail = async (req, res, next) => {
-  // const { email } = req.body;
-  // const user = new User({ email, confirmed: false });
-  // await user.save();
+	// const { email } = req.body;
+	// const user = new User({ email, confirmed: false });
+	// await user.save();
 
-  console.log("email Sending");
+	console.log("email Sending");
 
-  const email = "ghservicehub@gmail.com";
-  const name = "Frank Thomas";
-  const confirmationCode = uuid.v4();
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+	const email = "ghservicehub@gmail.com";
+	const name = "Frank Thomas";
+	const confirmationCode = uuid.v4();
+	const transporter = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		secure: false, // true for 465, false for other ports
+		auth: {
+			user: process.env.SMTP_USERNAME,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	});
 
-  const html = `
+	const html = `
     <html>
       <head>
         <style>
@@ -172,96 +191,96 @@ exports.sendMail = async (req, res, next) => {
     </html>
   `;
 
-  const mailOptions = {
-    from: "ServiceHub <dennisagbokpe@gmail.com>",
-    to: email, // list of receivers
-    subject: "Confirm your registration", // Subject line
-    html, // html body
-  };
+	const mailOptions = {
+		from: "ServiceHub <dennisagbokpe@gmail.com>",
+		to: email, // list of receivers
+		subject: "Confirm your registration", // Subject line
+		html, // html body
+	};
 
-  await transporter.sendMail(mailOptions);
-  res.send("Successfully Sent");
-  console.log("email Sent");
+	await transporter.sendMail(mailOptions);
+	res.send("Successfully Sent");
+	console.log("email Sent");
 };
 
 //Confirm / Verify user
 exports.ConfirmAdmin = async (req, res, next) => {
-  console.log("confirming user");
+	console.log("confirming user");
 };
 
 //Get All Admins
 exports.getAdmins = async (req, res, next) => {
-  console.log("get admin handler");
-  const admins = await Admin.find({}).sort({ createdAt: -1 });
-  res.status(200).json(admins);
+	console.log("get admin handler");
+	const admins = await Admin.find({}).sort({ createdAt: -1 });
+	res.status(200).json(admins);
 };
 
 //Get a Single Admin
 exports.getAdmin = async (req, res, next) => {
-  console.log("get an admin ");
-  const { id } = req.params;
+	console.log("get an admin ");
+	const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
-  const admin = await Admin.findById(id);
-  if (!admin) {
-    return res.status(404).json({ error: "Admin account not found" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
+	const admin = await Admin.findById(id);
+	if (!admin) {
+		return res.status(404).json({ error: "Admin account not found" });
+	}
 
-  res.status(200).json(admin);
+	res.status(200).json(admin);
 };
 
 // Change Admin Role
 exports.changeAdminRole = async (req, res) => {
-  console.log("change admin role");
+	console.log("change admin role");
 
-  const { id } = req.params;
+	const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
 
-  const admin = await Admin.findByIdAndUpdate(id, { role: req.body.role });
+	const admin = await Admin.findByIdAndUpdate(id, { role: req.body.role });
 
-  if (admin) {
-    return res.status(200).json(admin);
-  }
+	if (admin) {
+		return res.status(200).json(admin);
+	}
 };
 
 //Update Admin
 exports.updateAdmin = async (req, res, next) => {
-  console.log("update an Admin");
-  const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
+	console.log("update an Admin");
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
 
-  const admin = await Admin.findOneAndUpdate(
-    { _id: id },
-    {
-      ...req.body,
-    },
-  );
-  if (!admin) {
-    return res.status(400).json({ error: "No data Found" });
-  }
+	const admin = await Admin.findOneAndUpdate(
+		{ _id: id },
+		{
+			...req.body,
+		}
+	);
+	if (!admin) {
+		return res.status(400).json({ error: "No data Found" });
+	}
 
-  res.status(200).json(admin);
+	res.status(200).json(admin);
 };
 //
 
 //Delete an Admin
 exports.deleteAdmin = async (req, res, next) => {
-  const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
 
-  const admin = await Admin.findOneAndDelete({ _id: id });
-  if (!admin) {
-    return res.status(400).json({ error: "Admin Account not found" });
-  }
+	const admin = await Admin.findOneAndDelete({ _id: id });
+	if (!admin) {
+		return res.status(400).json({ error: "Admin Account not found" });
+	}
 
-  res.status(200).json(admin);
+	res.status(200).json(admin);
 };
--- routes/users.handlers.js
+++ routes/users.handlers.js
@@ -9,10 +9,10 @@ const path = require("path");
 const ejs = require("ejs");
 
 const emailTemplatePath = path.join(
-  __dirname,
-  "..",
-  "templates",
-  "confirmation-email.hbs",
+	__dirname,
+	"..",
+	"templates",
+	"confirmation-email.hbs"
 );
 
 //
@@ -22,257 +22,276 @@ const compiledEmailTemplate = Handlebars.compile(emailTemplate);
 
 //Login
 exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return Util.error("All fields required", next);
-    }
+	try {
+		const { email, password } = req.body;
+		if (!email || !password) {
+			return Util.error("All fields required", next);
+		}
 
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ message: "The email or password you entered is incorrect" });
-    }
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res
+				.status(400)
+				.json({ message: "The email or password you entered is incorrect" });
+		}
 
-    const matching = await user.authenticate(password);
-    if (!matching) {
-      return res
-        .status(400)
-        .json({ message: "The email or password you entered is incorrect" });
-    }
+		const matching = await user.authenticate(password);
+		if (!matching) {
+			if (user.login_attempts < 5) {
+				// record the login_attempts
+				User.updateOne(
+					{ id: user._id },
+					{ login_attempts: user.login_attempts + 1 }
+				);
 
-    // req.session.userId = user._id;
-    const { password: pass, rest } = user;
-    return res.status(200).json(rest);
-  } catch (error) {
-    return res.status(error.status || 401).json({ message: error.message });
-  }
+				return res
+					.status(400)
+					.json({ message: "The email or password you entered is incorrect" });
+			} else {
+				return res.status(400).json({
+					message:
+						"Too many failed login attempts. Please try again after 30 minutes",
+				});
+			}
+		}
+
+		// record the last_login
+		User.updateOne(
+			{ id: user._id },
+			{ login_attempts: 0, last_login: Date.now() }
+		);
+
+		// req.session.userId = user._id;
+		const { password: pass, login_attempts: loginAttempts, rest } = user;
+		return res.status(200).json(rest);
+	} catch (error) {
+		return res.status(error.status || 401).json({ message: error.message });
+	}
 };
 
 //Setting Up Multer MiddleWare
 const storage = multer.diskStorage({
-  destination: "./public/uploads/",
-  filename: function (req, file, cb) {
-    cb(null, uuid.v4() + "-" + file.originalname);
-  },
+	destination: "./public/uploads/",
+	filename: function (req, file, cb) {
+		cb(null, uuid.v4() + "-" + file.originalname);
+	},
 });
 const upload = multer({ storage: storage });
 
 exports.createUser = async (req, res, next) => {
-  console.log("Create user handler");
-  try {
-    upload.single("image")(req, res, async function (err) {
-      if (err) {
-        return res.status(400).json({
-          error: err.message,
-          cMsg: "Error creating user",
-        });
-      }
+	console.log("Create user handler");
+	try {
+		upload.single("image")(req, res, async function (err) {
+			if (err) {
+				return res.status(400).json({
+					error: err.message,
+					cMsg: "Error creating user",
+				});
+			}
 
-      //Assigning file properties
-      const file = req.file;
-      const baseUrl = `${req.protocol}://${req.headers.host}`;
-      const imageUrl = `${baseUrl}/uploads/${file?.filename}`; // get the path of the uploaded image
+			//Assigning file properties
+			const file = req.file;
+			const baseUrl = `${req.protocol}://${req.headers.host}`;
+			const imageUrl = `${baseUrl}/uploads/${file?.filename}`; // get the path of the uploaded image
 
-      const { email, phone } = req.body;
+			const { email, phone } = req.body;
 
-      // Checking if email or phone already exists
-      const emailExists = await User.findOne({ email });
-      if (emailExists) {
-        return res.status(400).json({
-          error: "Email already exists",
-          cMsg: "Error creating user",
-        });
-      }
-      //Checking if phone exists
-      const phoneExists = await User.findOne({ phone });
-      if (phoneExists) {
-        return res.status(400).json({
-          error: "Phone number already exists",
-          cMsg: "Error creating user",
-        });
-      }
+			// Checking if email or phone already exists
+			const emailExists = await User.findOne({ email });
+			if (emailExists) {
+				return res.status(400).json({
+					error: "Email already exists",
+					cMsg: "Error creating user",
+				});
+			}
+			//Checking if phone exists
+			const phoneExists = await User.findOne({ phone });
+			if (phoneExists) {
+				return res.status(400).json({
+					error: "Phone number already exists",
+					cMsg: "Error creating user",
+				});
+			}
 
-      const userData = req.body;
-      const verificationToken = uuid.v4();
-      const rememberToken = uuid.v4();
-      const user = await User.create({
-        ...userData,
-        image: file?.filename,
-        imageUrl,
-        verification_token: verificationToken,
-        remember_token: rememberToken,
-      });
+			const userData = req.body;
+			const verificationToken = uuid.v4();
+			const rememberToken = uuid.v4();
+			const user = await User.create({
+				...userData,
+				image: file?.filename,
+				imageUrl,
+				verification_token: verificationToken,
+				remember_token: rememberToken,
+			});
 
-      //Emailing the user
-      console.log("email Sending");
-      const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: process.env.SMTP_USERNAME,
-          pass: process.env.SMTP_PASSWORD,
-        },
-      });
+			//Emailing the user
+			console.log("email Sending");
+			const transporter = nodemailer.createTransport({
+				host: process.env.SMTP_HOST,
+				port: process.env.SMTP_PORT,
+				secure: false, // true for 465, false for other ports
+				auth: {
+					user: process.env.SMTP_USERNAME,
+					pass: process.env.SMTP_PASSWORD,
+				},
+			});
 
-      const mailOptions = {
-        from: "ServiceHub <dennisagbokpe@gmail.com>",
-        to: email, // list of receivers
-        subject: "Please Confirm Your Email Address", // Subject line
-        html: compiledEmailTemplate({
-          firstName: userData.firstName,
-          confirmUrl: `${baseUrl}/confirm?token=${user.verification_token}`,
-        }), // html body
-      };
+			const mailOptions = {
+				from: "ServiceHub <dennisagbokpe@gmail.com>",
+				to: email, // list of receivers
+				subject: "Please Confirm Your Email Address", // Subject line
+				html: compiledEmailTemplate({
+					firstName: userData.firstName,
+					confirmUrl: `${baseUrl}/confirm?token=${user.verification_token}`,
+				}), // html body
+			};
 
-      await transporter.sendMail(mailOptions);
-      console.log("email Sent");
+			await transporter.sendMail(mailOptions);
+			console.log("email Sent");
 
-      res.status(200).json(user);
-    });
-  } catch (error) {
-    next(error);
-  }
+			res.status(200).json(user);
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 //
 exports.confirmUserEmail = async (req, res, next) => {
-  console.log("Confirming User");
-  try {
-    const { token } = req.query;
-    const user = await User.findOne({ verification_token: token });
+	console.log("Confirming User");
+	try {
+		const { token } = req.query;
+		const user = await User.findOne({ verification_token: token });
 
-    if (!user) {
-      return res.status(400).json({
-        error: "Invalid token, please contact admin",
-      });
-    }
+		if (!user) {
+			return res.status(400).json({
+				error: "Invalid token, please contact admin",
+			});
+		}
 
-    user.is_email_verified = true;
-    user.verification_token += " Verified";
-    await user.save();
+		user.is_email_verified = true;
+		user.verification_token += " Verified";
+		await user.save();
 
-    // Render the successful activation page using EJS
-    const viewPath = path.join(
-      __dirname,
-      "../views/emailActivationSuccess.ejs",
-    );
-    const html = await ejs.renderFile(viewPath);
+		// Render the successful activation page using EJS
+		const viewPath = path.join(
+			__dirname,
+			"../views/emailActivationSuccess.ejs"
+		);
+		const html = await ejs.renderFile(viewPath);
 
-    res.status(200).send(html);
-  } catch (error) {
-    next(error);
-  }
+		res.status(200).send(html);
+	} catch (error) {
+		next(error);
+	}
 };
 
 //just testing Ejs Pages
 exports.ejsPage = async (req, res, next) => {
-  console.log("EJS Page Test");
-  const viewPath = path.join(__dirname, "../views/emailActivationSuccess.ejs");
-  const html = await ejs.renderFile(viewPath);
+	console.log("EJS Page Test");
+	const viewPath = path.join(__dirname, "../views/emailActivationSuccess.ejs");
+	const html = await ejs.renderFile(viewPath);
 
-  res.status(200).send(html);
+	res.status(200).send(html);
 };
 
 exports.verifySuccess = async (req, res, next) => {
-  console.log("Successful Activation");
-  res.send("Thank you for verifying your email!");
+	console.log("Successful Activation");
+	res.send("Thank you for verifying your email!");
 };
 
 //
 //Get All Users
 exports.getUsers = async (req, res, next) => {
-  console.log("get users handler");
-  const users = await User.find({}).sort({ createdAt: -1 });
-  res.status(200).json(users);
+	console.log("get users handler");
+	const users = await User.find({}).sort({ createdAt: -1 });
+	res.status(200).json(users);
 };
 
 //get a Single user
 exports.getUser = async (req, res, next) => {
-  console.log("get a user ");
-  const { id } = req.params;
+	console.log("get a user ");
+	const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
-  const user = await User.findById(id);
-  if (!user) {
-    return res.status(404).json({ error: "No user Found" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
+	const user = await User.findById(id);
+	if (!user) {
+		return res.status(404).json({ error: "No user Found" });
+	}
 
-  res.status(200).json(user);
+	res.status(200).json(user);
 };
 //
 
 //Update user
 exports.updateUser = async (req, res, next) => {
-  console.log("update a user ");
-  const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
+	console.log("update a user ");
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
 
-  const user = await User.findOneAndUpdate(
-    { _id: id },
-    {
-      ...req.body,
-    },
-  );
-  if (!user) {
-    return res.status(400).json({ error: "No data Found" });
-  }
+	const user = await User.findOneAndUpdate(
+		{ _id: id },
+		{
+			...req.body,
+		}
+	);
+	if (!user) {
+		return res.status(400).json({ error: "No data Found" });
+	}
 
-  res.status(200).json(user);
-  console.log(id);
+	res.status(200).json(user);
+	console.log(id);
 };
 //
 //Delete a user
 exports.deleteUser = async (req, res, next) => {
-  const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such data:id" });
-  }
+	const { id } = req.params;
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: "No such data:id" });
+	}
 
-  const user = await User.findOneAndDelete({ _id: id });
-  if (!user) {
-    return res.status(400).json({ error: "No user Found" });
-  }
+	const user = await User.findOneAndDelete({ _id: id });
+	if (!user) {
+		return res.status(400).json({ error: "No user Found" });
+	}
 
-  res.status(200).json(user);
+	res.status(200).json(user);
 };
 
 //
 exports.resetPassword = async (req, res, next) => {
-  console.log("Resetting Password");
-  try {
-    // Extract email and new password from request body
-    const { email, password } = req.body;
+	console.log("Resetting Password");
+	try {
+		// Extract email and new password from request body
+		const { email, password } = req.body;
 
-    // Check if user with email exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ error: "User not found" });
-    }
+		// Check if user with email exists
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ error: "User not found" });
+		}
 
-    // Use changePassword method to update user's password
-    user.changePassword(password, (err) => {
-      if (err) {
-        return res.status(500).json({ error: "Password update failed" });
-      }
+		// Use changePassword method to update user's password
+		user.changePassword(password, (err) => {
+			if (err) {
+				return res.status(500).json({ error: "Password update failed" });
+			}
 
-      //Reset user's reset password token
+			//Reset user's reset password token
 
-      //user.reset_password_token = null;
-      user.save();
+			//user.reset_password_token = null;
+			user.save();
 
-      // Send password reset confirmation email
+			// Send password reset confirmation email
 
-      // Return success response
-      return res.status(200).json({ message: "Password reset successful" });
-    });
-  } catch (error) {
-    next(error);
-  }
+			// Return success response
+			return res.status(200).json({ message: "Password reset successful" });
+		});
+	} catch (error) {
+		next(error);
+	}
 };
